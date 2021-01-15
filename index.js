3c5048e19ca9492af45c2074e06fd2b1845dc709
rafaelmrdyn <rafaelmrdyn@gmail.com>
2021-01-15 15:10:25 +0400

    make it better

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const bodyParser = require('body-parser');
+const composeAPI = require('@iota/core');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+const server = http.createServer(app);
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+const iota = composeAPI.composeAPI({
+    provider: 'https://nodes.devnet.iota.org:443'
+});
+
+app.get('/', function (req, res) {
+    res.send('iota core apis!!!')
+});
+
+app.get('/balances/:address', async function (req, res) {
+    try {
+        const { balances } = await iota.getBalances([req.params.address])
+        res.send({ balances })
+    } catch (err) {
+        res.status(400).send({
+            message: err.message
+        })
+    }
+});
+
+app.get('/transactions/:address', async function (req, res) {
+    try {
+        const transactions = await iota.findTransactionObjects({ addresses: [req.params.address] })
+        res.send({ transactions })
+    } catch (err) {
+        res.status(400).send({
+            message: err.message
+        })
+    }
+});
+
+server.listen(3000, function () {
+    console.log('listening on *:3000');
+});
